8fdf1352dcecaef641d8daab52efa37a2df52e4a
Willis911 <147372352+Willis911@users.noreply.github.com>
2024-12-04 14:10:10 +0300

    𝐓𝐑𝐀𝐂𝐄 𝐌𝐃

--- set.js
+++ set.js
@@ -14,7 +14,7 @@ module.exports = {
     session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>',
     PREFIXES: (process.env.PREFIX || '.').split(',').map(prefix => prefix.trim()).filter(Boolean),
     OWNER_NAME: process.env.OWNER_NAME || "Willis-Xth",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "254786273945",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "254786273945,254702365210",
     AUTO_READ_STATUS: process.env.AUTO_VIEW_STATUS || "on",
     AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "off",
     CHATBOT: process.env.CHAT_BOT || "off",
